a9975f15e18cebccf3b520cc0864e3cbbcbb914e
alan890104 <alan890104@gmail.com>
2023-08-23 12:04:06 +0800

    feat: use sequential

--- dqn-lunarlander.py
+++ dqn-lunarlander.py
@@ -38,15 +38,16 @@ class ReplayMemory:
 class Net(nn.Module):
     def __init__(self, state_dim=8, action_dim=4, hidden_dim=64):
         super().__init__()
-        self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, action_dim)
+        self.nn = nn.Sequential(
+            nn.Linear(state_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, action_dim),
+        )
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        return self.fc3(x)
-
+        return self.nn(x)
 
 class DQN:
     def __init__(self, args):
